3edfdd8c75e48080c655a0e581253b8726bcc102
lizonghao <794093538@qq.com>
2019-07-21 22:58:31 +0800

    feat: bug fix

--- bin/www.js
+++ bin/www.js
@@ -2,5 +2,4 @@ const http = require('http');
 const serverHandle = require('../app.js');
 
 const port = 8080;
-const server = http.createServer(serverHandle);
-server.listen(port);
+const server = http.creat
--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,61 +1,63 @@
-const {exec} = require('../db/mysql')
+const { exec } = require("../db/mysql");
 
 const getList = (author, keyword) => {
-    let sql = `select * from blog where isdel = 0`
-    if (author) {
-        sql += `and author = ${author}`
-    }
-    if (keyword) {
-        sql += `and title like '%${keyword}%' `
-    }
-    sql += ` order by createtime desc;`
-   
-    // 返回promise
-    return exec(sql);
-}
+  let sql = `select * from blog where isdel = 0`;
+  if (author) {
+    sql += `and author = ${author}`;
+  }
+  if (keyword) {
+    sql += `and title like '%${keyword}%' `;
+  }
+  sql += ` order by createtime desc;`;
 
-const getDetail = (id) => {
-    let sql = `select * from blog  where isdel = 0 and id = ${id}`
+  // 返回promise
+  return exec(sql);
+};
 
-    // 返回promise
-    return exec(sql).then(rows => rows[0]);
-}
+const getDetail = id => {
+  let sql = `select * from blog  where isdel = 0 and id = ${id}`;
+
+  // 返回promise
+  return exec(sql).then(rows => rows[0]);
+};
 
 const newBlog = (blogData = {}) => {
-    // blog对象
-    const {author, title, content} = blogData
-    const time = '2019-03-27 00:00:00';
-    const sql = `insert into blog (title, content, createtime, updatetime, author)
-     values ('${title}', '${content}', '${time}', '${time}', '${author}')`
-    return exec(sql).then(data =>({id: data.insertId}))
-}
+  // blog对象
+  const { author, title, content } = blogData;
+  const time = "2019-03-27 00:00:00";
+  const sql = `insert into blog (title, content, createtime, updatetime, author)
+     values ('${title}', '${content}', '${time}', '${time}', '${author}')`;
+  return exec(sql).then(data => ({ id: data.insertId }));
+};
 
 const updateBlog = (id, blogData = {}) => {
-    // 更新博客
-    const sql = `update blog set title = '${blogData.title}', content = '${blogData.content}', 
-    updatetime = '2019-03-27 00:00:00' where id = ${id} and isdel = 0`
-    return exec(sql).then(data => {
-        if (data.affectedRows > 0) {
-            return true
-        }
-        return false
-    })
-}
+  // 更新博客
+  const sql = `update blog set title = '${blogData.title}', content = '${
+    blogData.content
+  }', 
+    updatetime = '2019-03-27 00:00:00' where id = ${id} and isdel = 0`;
+  return exec(sql).then(data => {
+    if (data.affectedRows > 0) {
+      return true;
+    }
+    return false;
+  });
+};
 
 const delBlog = (id, author) => {
-    const sql = `update blog set isdel = 1 where id = ${id} and author = ${author}`
-    return exec(sql).then(data => {
-        if (data.affectedRows > 0) {
-            return true
-        }
-        return false
-    })
-    return true
-}
+  const sql = `update blog set isdel = 1 where id = ${id} and author = ${author}`;
+  return exec(sql).then(data => {
+    if (data.affectedRows > 0) {
+      return true;
+    }
+    return false;
+  });
+  return true;
+};
 module.exports = {
-    getList,
-    getDetail,
-    newBlog,
-    updateBlog,
-    delBlog
-}
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
+};
